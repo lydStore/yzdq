6659c7914ee636b7ee82e833db599c281694c10c
liyadong <liyadong@srccwl.com>
2020-09-16 17:16:53 +0800

    update

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -2,7 +2,7 @@
 	<div class="footer">
 		<div class="w1200" >
       <p>公司联系电话：<a href="tel:15670982182">15670982182</a> 邮箱：<a href="mailto:lyz102@yeah.net">lyz102@yeah.net</a></p>
-      <p><a href="http://www.yzdqservice.com/static/yzdq.png">经营许可证</a></p>
+      <p><a href="http://www.yzdqservice.com/static/yzdq.png" target="_blank">经营许可证</a></p>
       <p><a href="http://www.beian.miit.gov.cn" target="_blank">豫ICP备18032866号-1</a></p>
 			<P>公司名称：河南雨点机器人技术有限公司</p>
 			<p>公司地址：河南省郑州市高新技术产业开发区金梭路202号2幢1单元22层78号</p>
